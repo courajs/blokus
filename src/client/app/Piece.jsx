1be85cec52bb11c63a648794ed796263b40de968
Aaron Sikes <aaron@sikes.io>
2016-03-13 16:10:42 -0400

    Add the rest of the pieces

--- src/client/app/Piece.jsx
+++ src/client/app/Piece.jsx
@@ -3,14 +3,77 @@ import Blok from './Blok.jsx';
 import Spacer from './Spacer.jsx';
 
 export const pieceTypes = [
-  [[true]],                   //    x
+  [[true]],                     //  x
 
-  [[true, true]],             //    xx
+  [[true, true]],               //  xx
 
-  [[true, true, true]],       //    xxx
+  [[true, true, true]],         //  xxx
 
-  [[true, false],             //    x
-  [true, true]]               //    xx
+  [[true, false],               //  x
+  [true, true]],                //  xx
+
+  [[true, true],                //  xx
+  [true, true]],                //  xx
+
+  [[true, true, false],         //  xx
+  [false, true, true]],         //   xx
+
+  [[true, false],               //  x
+  [true, false],                //  x
+  [true, true]],                //  xx
+
+  [[true, true, true],          //  xxx
+  [false, true, false]],        //   x
+
+  [[true],                      //  x
+  [true],                       //  x
+  [true],                       //  x
+  [true]],                      //  x
+
+  [[false, true, true],         //   xx
+  [true, true, false],          //  xx
+  [false, true, false]],        //   x
+
+  [[true, false],               //  x
+  [true, false],                //  x
+  [true, false],                //  x
+  [true, true]],                //  xx
+
+  [[true],                      //  x
+  [true],                       //  x
+  [true],                       //  x
+  [true],                       //  x
+  [true]],                      //  x
+
+  [[true, true],                //  xx
+  [true, true],                 //  xx
+  [true, false]],               //  x
+
+  [[true, true, true],          //  xxx
+  [false, true, false],         //   x
+  [false, true, false]],        //   x
+
+  [[true, false, true],         //  x x
+  [true, true, true]],          //  xxx
+  
+  [[false, false, true],        //    x
+  [false, false, true],         //    x
+  [true, true, true]],          //  xxx
+
+  [[false, false, true],        //    x
+  [false, true, true],          //   xx
+  [true, true, false]],         //  xx
+
+  [[false, true, false],        //   x
+  [true, true, true],           //  xxx
+  [false, true, false]],        //   x
+
+  [[false, true, false, false], //    x
+  [true, true, true, true]],    //  xxxx
+
+  [[true, true, false],         //  xx
+  [false, true, false],         //   x
+  [false, true, true]]          //   xx
 ];
 
 
